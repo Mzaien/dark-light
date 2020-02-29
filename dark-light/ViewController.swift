65a52bfc595834bfdafe54d26d3e7aeceb45fa3b
Mzaien <Awmzien@gmail.com>
2020-02-29 07:38:12 +0300

    Made it :100:
    from one commite

--- dark-light/ViewController.swift
+++ dark-light/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var isoff = true
+    @IBOutlet weak var light:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+   
+    @IBAction func buttonpressed(_ sender: Any) {
+        if isoff == true {
+            view.backgroundColor = .white
+            light.text = "lights off"
+            light.textColor = .black
+            isoff = false
+        }else{
+            view.backgroundColor = .black
+            light.text = "lights on"
+            light.textColor = .white
+            isoff = true
+        }
+    }
 }
 
